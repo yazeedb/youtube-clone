39394bf1a633f9ef86cad52da37f87185e64b121
yazeedb <ybzadough@gmail.com>
2017-11-13 23:37:24 -0500

    add video, search, and account icons

--- src/app/shared/nav/index.js
+++ src/app/shared/nav/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import Avatar from 'material-ui/Avatar';
 import IconButton from 'material-ui/IconButton';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
-import FontIcon from 'material-ui/FontIcon';
+import IconMenu from 'material-ui/IconMenu';
+import Person from 'material-ui/svg-icons/social/Person';
+import Search from 'material-ui/svg-icons/action/Search';
+import Videocam from 'material-ui/svg-icons/av/Videocam';
+
 import './nav.scss';
 
 export default () => (
@@ -14,6 +18,19 @@ export default () => (
       <IconButton>
         <span className="youtube-logo" />
       </IconButton>
+    }
+    iconElementRight={
+      <div>
+        <IconButton>
+          <Videocam />
+        </IconButton>
+        <IconButton>
+          <Search />
+        </IconButton>
+        <IconButton>
+          <Person />
+        </IconButton>
+      </div>
     }
   />
 );
