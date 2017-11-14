719341dc3472936029804452b8339a2b9255e4f3
yazeedb <ybzadough@gmail.com>
2017-11-13 22:18:56 -0500

    add scss and png loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const webpack = require('webpack');
 const { resolve } = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 const root = (...args) => resolve(process.cwd(), ...args);
 
 module.exports = {
@@ -23,7 +25,22 @@ module.exports = {
             plugins: ['transform-object-rest-spread']
           }
         }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.png/,
+        use: [
+          'url-loader?limit=10000',
+          'img-loader'
+        ]
       }
     ]
-  }
+  },
+  plugins: [new ExtractTextPlugin({ filename: '[name].css' })]
 };
