2e9e5a8d3e2f7b762063db2c98a73bccef55a538
yazeedb <ybzadough@gmail.com>
2017-11-13 23:59:42 -0500

    add bottom nav

--- src/app/home/index.js
+++ src/app/home/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Nav from '../shared/nav';
+import BottomNav from '../shared/bottom-nav';
 
 const Home = () => (
   <div className="home">
     <Nav />
+    <BottomNav />
   </div>
 );
 
---
+++ src/app/shared/bottom-nav/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BottomNavigation, BottomNavigationItem } from 'material-ui/BottomNavigation';
+import Home from 'material-ui/svg-icons/action/home';
+import WhatsHot from 'material-ui/svg-icons/social/whatshot';
+import Subscriptions from 'material-ui/svg-icons/av/subscriptions';
+import Folder from 'material-ui/svg-icons/file/folder';
+import Paper from 'material-ui/Paper';
+
+export default () => (
+  <Paper zDepth={ 1 }>
+    <BottomNavigation>
+      <BottomNavigationItem label="Home" icon={ <Home /> } />
+      <BottomNavigationItem label="Trending" icon={ <WhatsHot /> } />
+      <BottomNavigationItem label="Subscriptions" icon={ <Subscriptions /> } />
+      <BottomNavigationItem label="Library" icon={ <Folder /> } />
+    </BottomNavigation>
+  </Paper>
+);
