df13eaf1b0dcd6a466a435a409e6ffb0540a898c
yazeedb <ybzadough@gmail.com>
2017-11-14 00:08:32 -0500

    theme bottomNav

--- src/app/theme.js
+++ src/app/theme.js
@@ -2,11 +2,15 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { white } from 'material-ui/styles/colors';
 
 const almostBlack = '#282828';
+const youtubeRed = '#FF0000';
 
 export default getMuiTheme({
   palette: {
     primary1Color: white,
     textColor: almostBlack,
     alternateTextColor: almostBlack
+  },
+  bottomNavigation: {
+    selectedColor: youtubeRed
   }
 });
