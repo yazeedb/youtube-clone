909ab65677b5e674ab8e3d7a85b1c0c11813c274
yazeedb <ybzadough@gmail.com>
2017-11-13 22:41:10 -0500

    add youtube logo

--- src/app/shared/nav/index.js
+++ src/app/shared/nav/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import FontIcon from 'material-ui/FontIcon';
 import './nav.scss';
 
 export default () => (
    <AppBar
+    title="YouTube"
     className="nav"
-    iconElementLeft={
-      <IconButton>
-        <span className="youtube-logo"></span>
-      </IconButton>
-    }
+        iconElementLeft={<IconButton><span className="youtube-logo" /></IconButton>}
   />
 );
