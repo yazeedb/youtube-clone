6adcbe9b7b15528e488de61f1a03019503a70a7f
yazeedb <ybzadough@gmail.com>
2017-11-13 23:05:53 -0500

    add MUI theme

---
+++ src/app/theme.js
@@ -0,0 +1,11 @@
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { white } from 'material-ui/styles/colors';
+
+const almostBlack = '#282828';
+
+export default getMuiTheme({
+  palette: {
+    primary1Color: white,
+    alternateTextColor: almostBlack
+  }
+});
