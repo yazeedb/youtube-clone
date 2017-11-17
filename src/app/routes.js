cf7dc89c05d40b8bf33f6b78d8116ea308bc7c77
yazeedb <ybzadough@gmail.com>
2017-11-16 21:21:41 -0500

    add trending page

--- src/app/routes.js
+++ src/app/routes.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Nav from './shared/nav';
 import BottomNav from './shared/bottom-nav';
 import Home from './home';
+import Trending from './trending';
 
 export default () => (
   <BrowserRouter>
     <div>
       <Nav />
         <Route exact path="/" component={ Home } />
+        <Route path="/trending" component={ Trending } />
       <BottomNav />
     </div>
   </BrowserRouter>
