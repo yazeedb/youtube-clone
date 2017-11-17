dc66d74258ad7e0f01a8da5eefa1745a42df4920
yazeedb <ybzadough@gmail.com>
2017-11-16 21:57:14 -0500

    add sample trending api endpoint

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const PORT = 8080;
 
 app.use(express.static(join(__dirname, 'dist')));
 
+app.get('/trending', (req, res) => {
+    const fakeTrendingData = require('./src/mock-json/trending.json');
+
+    res.status(200).send(fakeTrendingData);
+});
+
 app.get('*', (req, res) => {
   res.sendFile(join(__dirname, 'src/index.html'));
 });
