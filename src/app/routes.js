3d99d9ef577d053005650fbf6fafa3d980835d00
yazeedb <ybzadough@gmail.com>
2017-11-22 08:05:30 -0500

    add app.js

--- src/app/routes.js
+++ src/app/routes.js
@@ -3,16 +3,15 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import Nav from './shared/Nav';
 import BottomNav from './shared/BottomNav';
+import App from './App';
 import Home from './Home';
 import Trending from './Trending';
 
 export default () => (
   <BrowserRouter>
-    <div>
-      <Nav />
-        <Route exact path="/" component={ Home } />
-        <Route path="/trending" component={ Trending } />
-      <BottomNav />
-    </div>
+    <App>
+      <Route exact path="/" component={ Home } />
+      <Route exact path="/trending" component={ Trending } />
+    </App>
   </BrowserRouter>
 );
