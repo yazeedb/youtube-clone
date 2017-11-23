e9bbf606d65930f9390c5250380015c6bf436435
yazeedb <ybzadough@gmail.com>
2017-11-22 21:50:49 -0500

    add scrollable component

---
+++ src/components/shared/Scrollable/index.js
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+
+class Scrollable extends Component {
+	constructor(props) {
+		super(props);
+		this.onScroll = this.onScroll.bind(this);
+	}
+	componentWillMount() {
+		document.addEventListener('scroll', this.onScroll);
+	}
+
+	componentWillUnmount() {
+		document.removeEventListener('scroll', this.onScroll);
+	}
+
+	onScroll(e) {
+		console.log('my onscroll was called', e);
+
+		const { onScroll } = this.props;
+		onScroll(e);
+	}
+
+	render() {
+		return this.props.children;
+	}
+}
+
+export default Scrollable;
