0298996261fd21fba0db8fbdb32dd710f70ae9f7
yazeedb <ybzadough@gmail.com>
2017-11-22 22:34:40 -0500

    make sliding navbar

--- src/components/shared/Nav/index.js
+++ src/components/shared/Nav/index.js
@@ -1,40 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import AccountCircle from 'material-ui/svg-icons/action/account-circle';
 import Search from 'material-ui/svg-icons/action/Search';
 import Videocam from 'material-ui/svg-icons/av/Videocam';
+import Scrollable from 'shared/Scrollable';
 
 import './Nav.scss';
 
-const navStyle = {
+const getNavStyle = (scrollingDown) => ({
 	position: 'fixed',
-	top: 0,
-	left: 0
-};
+	top: scrollingDown ? '-50%' : 0,
+	left: 0,
+	transition: 'top 0.5s'
+});
 
-export default () => (
-	<AppBar
-		title="YouTube"
-		className="nav"
-		style={ navStyle }
-		iconElementLeft={
-			<IconButton>
-				<span className="youtube-logo" />
-			</IconButton>
-		}
-		iconElementRight={
-			<div>
-				<IconButton>
-					<Videocam />
-				</IconButton>
-				<IconButton>
-					<Search />
-				</IconButton>
-				<IconButton>
-					<AccountCircle />
-				</IconButton>
-			</div>
-		}
-	/>
-);
+class Nav extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			oldPageYOffset: window.pageYOffset,
+			scrollingDown: false
+		};
+
+		this.onScroll = this.onScroll.bind(this);
+	}
+
+	onScroll() {
+		const { oldPageYOffset } = this.state;
+		const currentPageYOffset = window.pageYOffset;
+		const scrollingDown = currentPageYOffset > oldPageYOffset;
+
+		console.log('oldPageYOffset:', oldPageYOffset);
+		console.log('currentPageYOffset:', currentPageYOffset);
+		console.log('scrollingDown:', scrollingDown);
+
+		this.setState({
+			scrollingDown,
+			oldPageYOffset: currentPageYOffset
+		});
+	}
+
+	render() {
+		const { scrollingDown } = this.state;
+
+		return (
+			<Scrollable onScroll={ this.onScroll }>
+				<AppBar
+					title="YouTube"
+					className="nav"
+					style={ getNavStyle(scrollingDown) }
+					iconElementLeft={
+						<IconButton>
+							<span className="youtube-logo" />
+						</IconButton>
+					}
+					iconElementRight={
+						<div>
+							<IconButton>
+								<Videocam />
+							</IconButton>
+							<IconButton>
+								<Search />
+							</IconButton>
+							<IconButton>
+								<AccountCircle />
+							</IconButton>
+						</div>
+					}
+				/>
+			</Scrollable>
+		);
+	}
+}
+
+export default Nav;
