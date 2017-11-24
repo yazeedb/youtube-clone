b6c6717ac452d0b9fe35c1053674beaa40fbb437
yazeedb <ybzadough@gmail.com>
2017-11-23 20:41:42 -0500

    fix linting errors

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const PORT = 3000;
 app.use(express.static(join(__dirname, 'dist')));
 
 app.get('/api/trending', (req, res) => {
-    const fakeTrendingData = require('./mock-json/trending.json');
+	const fakeTrendingData = require('./mock-json/trending.json');
 
-    res.status(200).send(fakeTrendingData);
+	res.status(200).send(fakeTrendingData);
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, 'src/index.html'));
+	res.sendFile(join(__dirname, 'src/index.html'));
 });
 
 app.listen(PORT, () => console.log('listening on port', PORT));
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,51 @@
-const webpack = require('webpack');
 const { resolve } = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const root = (...args) => resolve(process.cwd(), ...args);
 
 module.exports = {
-  output: {
-    path: root('dist'),
-    filename: 'bundle.js'
-  },
-  entry: root('src/index'),
-  resolve: {
-    extensions: ['.js'],
-    alias: {
-      actions: root('src/actions'),
-      reducers: root('src/reducers'),
-      components: root('src/components'),
-      shared: root('src/components/shared')
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['react'],
-            plugins: ['transform-object-rest-spread']
-          }
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
-      },
-      {
-        test: /\.png/,
-        use: [
-          'url-loader?limit=10000',
-          'img-loader'
-        ]
-      }
-    ]
-  },
-  plugins: [new ExtractTextPlugin({ filename: '[name].css' })]
+	output: {
+		path: root('dist'),
+		filename: 'bundle.js'
+	},
+	entry: root('src/index'),
+	resolve: {
+		extensions: ['.js'],
+		alias: {
+			actions: root('src/actions'),
+			reducers: root('src/reducers'),
+			components: root('src/components'),
+			shared: root('src/components/shared')
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['react'],
+						plugins: ['transform-object-rest-spread']
+					}
+				}
+			},
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'sass-loader']
+				})
+			},
+			{
+				test: /\.png/,
+				use: [
+					'url-loader?limit=10000',
+					'img-loader'
+				]
+			}
+		]
+	},
+	plugins: [new ExtractTextPlugin({ filename: '[name].css' })]
 };
