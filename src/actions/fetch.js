36dbf49046aea40b77fb35b5869cb6f242a16f79
yazeedb <ybzadough@gmail.com>
2017-11-28 20:14:44 -0500

    process videos on successful request

--- src/actions/fetch.js
+++ src/actions/fetch.js
@@ -1,14 +1,21 @@
 import fetch from 'isomorphic-fetch';
 import { setVideoList } from './videoList';
+import { processForDetailedList } from 'helpers/processVideo';
 
 export const fetchTrendingVideos = () => (dispatch) => (
 	fetch('/api/trending')
 		.then((res) => {
 			console.log('fetchTrendingVideos response:', res);
+
 			return res.json();
 		})
 		.then((json) => {
 			console.log('fetchTrendingVideos json:', json);
-			dispatch(setVideoList(json.items));
+
+			const processedVideos = json
+				.items
+				.map((vid) => processForDetailedList(vid));
+
+			dispatch(setVideoList(processedVideos));
 		})
 );
