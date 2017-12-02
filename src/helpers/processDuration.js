a663bd0fede525802b02683519c897ad8646eb3a
yazeedb <ybzadough@gmail.com>
2017-12-02 13:32:46 -0500

    remove useless duration format param

--- src/helpers/processDuration.js
+++ src/helpers/processDuration.js
@@ -2,6 +2,6 @@ import moment from 'moment';
 import 'moment-duration-format';
 
 export default (duration) => moment
-	.duration(duration, 'seconds')
+	.duration(duration)
 	.format('h:mm:ss')
 	.padStart(4, '0:0');
