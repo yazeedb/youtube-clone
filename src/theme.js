8c20dd2b451990346fdf4e2aeccb733884e29c5d
yazeedb <ybzadough@gmail.com>
2017-12-04 19:56:24 -0500

    export colors from theme

--- src/theme.js
+++ src/theme.js
@@ -2,8 +2,8 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { white } from 'material-ui/styles/colors';
 import Typography from 'material-ui/styles/typography';
 
-const almostBlack = '#282828';
-const youtubeRed = '#FF0000';
+export const almostBlack = '#282828';
+export const youtubeRed = '#FF0000';
 
 export default getMuiTheme({
 	palette: {
